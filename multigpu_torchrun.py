175d72c83061b2a57622532214657650a2712093
yh-raphael <yhcho.raphael@gmail.com>
2023-02-24 13:48:00 +0000

    first commit - writing multigpu_torchrun now

---
+++ multigpu_torchrun.py
@@ -0,0 +1,58 @@
+import torch
+import torch.nn.functional as F
+from torch.utils.data import Dataset, DataLoader
+from datautils import MyTrainDataset
+
+import torch.multiprocessing as mp
+from torch.utils.data.distributed import DistributedSampler
+from torch.nn.parallel import DistributedDataParallel as DDP
+from torch.distributed import init_process_group, destroy_process_group
+import os
+
+
+def ddp_setup(rank, world_size):
+    init_process_group(backend="nccl")
+
+class Trainer:
+    def __init__(
+        self,
+        model: torch.nn.Module,
+        train_data: DataLoader,
+        optimizer: torch.optim.Optimizer,
+        save_every: int,
+        snapshot_path: str,     ### [ADDED on torchrun-version code] ###
+    ) -> None:
+        self.gpu_id = gpu_id
+        self.model = model.to(gpu_id)
+        self.train_data = train_data
+        self.optimizer = optimizer
+        self.save_every = save_every
+        self.model = DDP (model, device_ids = [gpu_id])     ### [ADDED on multi-version code] ###
+
+    def _run_batch(self, source, targets):
+        self.optimizer.zero_grad()
+        output = self.model(source)
+        loss = F.cross_entropy(output, targets)
+        loss.backward()
+        self.optimizer.step()
+
+    def _run_epoch(self, epoch):
+        b_sz = len(next(iter(self.train_data))[0])
+        print(f"[GPU{self.gpu_id}] Epoch {epoch} | Batchsize: {b_sz} | Steps: {len(self.train_data)}")
+        self.train_data.sampler.set_epoch(epoch)        ### [ADDED on multi-version code] ###
+        for source, targets in self.train_data:
+            source = source.to(self.gpu_id)
+            targets = targets.to(self.gpu_id)
+            self._run_batch(source, targets)
+
+    def _save_checkpoint(self, epoch):
+        ckp = self.model.state_dict()
+        PATH = "checkpoint.pt"
+        torch.save(ckp, PATH)
+        print(f"Epoch {epoch} | Training checkpoint saved at {PATH}")
+
+    def train(self, max_epochs: int):
+        for epoch in range(max_epochs):
+            self._run_epoch(epoch)
+            if self.gpu_id == 0 and epoch % self.save_every == 0:            ### [FIXED on multi-version code] ### : self.gpu_id == 0
+                self._save_checkpoint(epoch)
